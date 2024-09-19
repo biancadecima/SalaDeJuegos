c5372e2fd9a9b79ddad8756b50e9f7c6b861127a
Bianca <bianbdecima@gmail.com>
2024-09-19 18:53:43 -0300

    pagenotfound component

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
+  mensaje: string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  back() {
+    this.router.navigate(['/home']);
+  }
 
 }
